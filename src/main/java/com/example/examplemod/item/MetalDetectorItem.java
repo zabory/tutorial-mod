5da10671553c25985f792506b50e60b8dadf0b48
BShabowski <benshabowski@gmail.com>
2023-09-22 20:44:15 -0500

    initial commit. End of tutorial 6

---
+++ src/main/java/com/example/examplemod/item/MetalDetectorItem.java
@@ -0,0 +1,60 @@
+package com.example.examplemod.item;
+
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MetalDetectorItem extends Item {
+    public MetalDetectorItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        if(!pContext.getLevel().isClientSide()){
+            BlockPos positionClicked = pContext.getClickedPos();
+            Player player = pContext.getPlayer();
+            boolean foundBlock = false;
+
+            for (int i = 0; i <= positionClicked.getY() + 64; i++) {
+                BlockState state = pContext.getLevel().getBlockState(positionClicked.below(i));
+                if(isValuableBlock(state)){
+                    outputValuableCoordinates(positionClicked.below(i), player, state.getBlock());
+                    foundBlock = true;
+                    break;
+                }
+            }
+
+            if(!foundBlock){
+                player.sendSystemMessage(Component.literal("No valuables found"));
+            }
+        }
+
+        pContext.getItemInHand().hurtAndBreak(1, pContext.getPlayer(),
+                player -> player.broadcastBreakEvent(player.getUsedItemHand()));
+
+        return InteractionResult.SUCCESS;
+    }
+
+    private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
+        player.sendSystemMessage(Component.literal("Found " + I18n.get(block.getDescriptionId()) + " at " +
+                "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"));
+    }
+
+    private boolean isValuableBlock(BlockState state) {
+        LinkedList<Block> valid = new LinkedList<>(
+                Arrays.asList(Blocks.IRON_ORE, Blocks.DIAMOND_ORE)
+        );
+        return valid.contains(state.getBlock());
+    }
+}
---
+++ src/main/java/com/example/examplemod/item/ModCreativeModeTabs.java
@@ -0,0 +1,39 @@
+package com.example.examplemod.item;
+
+import com.example.examplemod.TutorialMod;
+import com.example.examplemod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public abstract class ModCreativeModeTabs {
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MODID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.SAPPHIRE.get());
+                        pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+                        pOutput.accept(ModItems.METAL_DETECTOR.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
